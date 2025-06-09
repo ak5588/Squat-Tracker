441cd6649ce624598b95680aadbdad7b319cfb5a
ak5588 <alokkushwaha881@gmail.com>
2025-06-09 20:34:04 +0530

    Initialize with clean .gitignore (ignored venv)

---
+++ Demo.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+
+st.title('AI Fitness Trainer: Squats Analysis')
+
+recorded_file ='https://www.youtube.com/watch?v=YaXPRqUwItQ'
+sample_vid = st.empty()
+sample_vid.video(recorded_file)
+
+
+    
+    
+
+    
+
+
